293351e23c0dcc95d1611311e979127afeb54eef
JayabharathiJayaraman <jayabharathi.jayaraman@iths.se>
2021-11-11 09:50:51 +0100

    Firebase ReactNative

---
+++ services/firebaseService.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA4OqtAsDMm6HY68JBQl6VZD7Z7caeFXto",
+    authDomain: "fantasticexpoproject.firebaseapp.com",
+    databaseURL: "https://fantasticexpoproject-default-rtdb.firebaseio.com",
+    projectId: "fantasticexpoproject",
+    storageBucket: "fantasticexpoproject.appspot.com",
+    messagingSenderId: "130398239135",
+    appId: "1:130398239135:web:6910b827ea0b38c89125aa",
+    measurementId: "G-VGZNG8F8HD"
+  };
